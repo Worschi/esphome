2c6f2afca5ff4aa99092a3d236b3f35ebf5e3ad3
Worschi <lazerswilldo@gmail.com>
2022-10-16 21:50:58 +0200

    implement models

--- esphome/components/hrxl_maxsonar_wr/hrxl_maxsonar_wr.h
+++ esphome/components/hrxl_maxsonar_wr/hrxl_maxsonar_wr.h
@@ -4,8 +4,15 @@
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/uart/uart.h"
 
+
 namespace esphome {
 namespace hrxl_maxsonar_wr {
+
+ // Simple enum to represent models.
+ enum Model {
+   HRXL = 0,  // mm resolution models
+   XL = 1,    // cm resolution models
+ };
 
 class HrxlMaxsonarWrComponent : public sensor::Sensor, public Component, public uart::UARTDevice {
  public:
@@ -13,13 +20,14 @@ class HrxlMaxsonarWrComponent : public sensor::Sensor, public Component, public 
 
   // ========== INTERNAL METHODS ==========
   void setup() override;
+  void set_model(Model model) { this->model_ = model; };
   void loop() override;
   void dump_config() override;
 
  protected:
   void check_buffer_();
-
   std::string buffer_;
+  Model model_{HRXL};
 };
 
 }  // namespace hrxl_maxsonar_wr
